91375e2980491d65422af795310c2f7c293215fa
thecodingone <amanda.southworth@myds.us>
2017-03-08 11:36:19 -0800

    added the UIImageViews

--- planets.playground/Contents.swift
+++ planets.playground/Contents.swift
@@ -27,49 +27,50 @@ mercury.image = UIImage(named: "mercury.gif")
 mercury.contentMode =  UIViewContentMode.scaleAspectFill
 mercury.center.y = sun.center.y
 
-var venus = UIView(frame: CGRect(x: 280, y: mainSpace.frame.size.width / 2, width: 9, height: 9))
-venus.backgroundColor = UIColor.yellow
-venus.layer.cornerRadius = venus.frame.size.width / 2
+var venus = UIImageView(frame: CGRect(x: 280, y: mainSpace.frame.size.width / 2, width: 9, height: 9))
+venus.image = UIImage(named: "venus.gif")
+venus.contentMode = UIViewContentMode.scaleAspectFill
 venus.center.y = sun.center.y
 
-var earth = UIView(frame: CGRect(x: 310,y: mainSpace.frame.size.width / 2, width: 10, height: 10))
-earth.backgroundColor = UIColor.blue
-earth.layer.cornerRadius = venus.frame.size.width / 2
+var earth = UIImageView(frame: CGRect(x: 310,y: mainSpace.frame.size.width / 2, width: 10, height: 10))
+earth.image = UIImage(named: "earth.gif")
+earth.contentMode = UIViewContentMode.scaleAspectFill
 earth.center.y = sun.center.y
 
-var mars = UIView(frame: CGRect(x: 350 , y: mainSpace.frame.size.width / 2, width: 7, height: 7))
-mars.backgroundColor = UIColor.orange
-mars.layer.cornerRadius = venus.frame.size.width / 2
+var mars = UIImageView(frame: CGRect(x: 350 , y: mainSpace.frame.size.width / 2, width: 7, height: 7))
+mars.image = UIImage(named: "mars.gif")
+mars.contentMode = UIViewContentMode.scaleAspectFill
 mars.center.y = sun.center.y
 
-var jupiter = UIView(frame: CGRect(x: 400, y: mainSpace.frame.size.width / 2, width: 150, height: 150))
-jupiter.backgroundColor = UIColor.red
-jupiter.layer.cornerRadius = jupiter.frame.size.width / 2
+var jupiter = UIImageView(frame: CGRect(x: 400, y: mainSpace.frame.size.width / 2, width: 150, height: 150))
+jupiter.image = UIImage(named: "jupiter.gif")
+jupiter.contentMode = UIViewContentMode.scaleAspectFill
 jupiter.center.y = sun.center.y
 
-var saturn =  UIView(frame: CGRect(x: 600, y: mainSpace.frame.size.width / 2, width: 70, height: 70))
-saturn.backgroundColor = UIColor.yellow
-saturn.layer.cornerRadius = saturn.frame.size.width / 2
+var saturn =  UIImageView(frame: CGRect(x: 600, y: mainSpace.frame.size.width / 2, width: 100, height: 100))
+saturn.image = UIImage(named: "saturn.gif")
+saturn.contentMode = UIViewContentMode.scaleAspectFill
 saturn.center.y = sun.center.y
 
-var saturn_rings = UIView(frame: CGRect(x: 600, y: mainSpace.frame.size.width / 2, width: 90, height: 1))
-saturn_rings.backgroundColor = UIColor.gray 
+var saturn_rings = UIView(frame: CGRect(x: 600, y: mainSpace.frame.size.width / 2, width: 160, height: 1))
+saturn_rings.backgroundColor = UIColor(red:0.76, green:0.70, blue:0.69, alpha:1.0)
+saturn_rings.layer.cornerRadius = 7
 saturn_rings.center.y = saturn.center.y
 saturn_rings.center.x = saturn.center.x
 
-var uranus =  UIView(frame: CGRect(x: 720, y: mainSpace.frame.size.width / 2, width: 30, height: 30))
-uranus.backgroundColor = UIColor.green
-uranus.layer.cornerRadius = uranus.frame.size.width / 2
+var uranus =  UIImageView(frame: CGRect(x: 770, y: mainSpace.frame.size.width / 2, width: 30, height: 30))
+uranus.image = UIImage(named: "uranus.gif")
+uranus.contentMode = UIViewContentMode.scaleAspectFill
 uranus.center.y = sun.center.y
 
-var neptune =  UIView(frame: CGRect(x: 820, y: mainSpace.frame.size.width / 2, width: 25, height: 25))
-neptune.backgroundColor = UIColor.blue
-neptune.layer.cornerRadius = neptune.frame.width / 2
+var neptune =  UIImageView(frame: CGRect(x: 860, y: mainSpace.frame.size.width / 2, width: 25, height: 25))
+neptune.image = UIImage(named: "neptune.gif")
+neptune.contentMode = UIViewContentMode.scaleAspectFill
 neptune.center.y = sun.center.y
 
-var pluto =  UIView(frame: CGRect(x: 920, y: mainSpace.frame.size.width / 2, width: 5, height: 5))
-pluto.backgroundColor = UIColor.brown
-pluto.layer.cornerRadius = pluto.frame.width / 2
+var pluto =  UIImageView(frame: CGRect(x: 930, y: mainSpace.frame.size.width / 2, width: 4, height: 4))
+pluto.image = UIImage(named: "pluto.gif")
+pluto.contentMode = UIViewContentMode.scaleAspectFill
 pluto.center.y = sun.center.y
 
 mainSpace.addSubview(sun)
