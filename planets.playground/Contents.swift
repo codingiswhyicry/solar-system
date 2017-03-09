a84fd0c14dc4777b599f91930f4a2c3fe8f15375
thecodingone <amanda.southworth@myds.us>
2017-03-09 12:44:49 -0800

    added moon

--- planets.playground/Contents.swift
+++ planets.playground/Contents.swift
@@ -13,22 +13,27 @@ sun.contentMode =  UIViewContentMode.scaleAspectFill
 sun.center.y = mainSpace.frame.height / 2
 sun.center.x = mainSpace.frame.width / 2
 
-var mercury = UIImageView(frame: CGRect(x: mainSpace.frame.height / 2, y: mainSpace.frame.size.width / 2 - 130, width: 5, height: 5))
+var mercury = UIImageView(frame: CGRect(x: mainSpace.frame.height / 2, y: mainSpace.frame.size.width / 2 - 130, width: 9, height: 9))
 mercury.image = UIImage(named: "mercury.gif")
 mercury.contentMode =  UIViewContentMode.scaleAspectFill
 mercury.center.x = sun.center.x
 
-var venus = UIImageView(frame: CGRect(x: mainSpace.frame.height / 2, y: mainSpace.frame.size.width / 2 - 180, width: 9, height: 9))
+var venus = UIImageView(frame: CGRect(x: mainSpace.frame.height / 2, y: mainSpace.frame.size.width / 2 - 180, width: 17, height: 17))
 venus.image = UIImage(named: "venus.gif")
 venus.contentMode = UIViewContentMode.scaleAspectFill
 venus.center.x = sun.center.x
 
-var earth = UIImageView(frame: CGRect(x: mainSpace.frame.height /  2,y: mainSpace.frame.size.width / 2 - 230, width: 11, height: 11))
+var earth = UIImageView(frame: CGRect(x: mainSpace.frame.height /  2, y: mainSpace.frame.size.width / 2 - 230, width: 20, height: 20))
 earth.image = UIImage(named: "earth.gif")
 earth.contentMode = UIViewContentMode.scaleAspectFill
 earth.center.x = venus.center.x
 
-var mars = UIImageView(frame: CGRect(x: mainSpace.frame.height /  2, y: mainSpace.frame.size.width / 2 - 280, width: 9, height: 9))
+var moon = UIImageView(frame: CGRect(x: mainSpace.frame.height / 2, y: mainSpace.frame.size.width / 2 - 260, width: 5, height: 5))
+moon.image = UIImage(named: "moon.gif")
+moon.contentMode = UIViewContentMode.scaleAspectFill
+moon.center.x = sun.center.x
+
+var mars = UIImageView(frame: CGRect(x: mainSpace.frame.height /  2, y: mainSpace.frame.size.width / 2 - 280, width: 12, height: 12))
 mars.image = UIImage(named: "mars.gif")
 mars.contentMode = UIViewContentMode.scaleAspectFill
 mars.center.x = sun.center.x
@@ -44,7 +49,7 @@ saturn.contentMode = UIViewContentMode.scaleAspectFill
 saturn.center.x = sun.center.x
 
 
-var uranus =  UIImageView(frame: CGRect(x: mainSpace.frame.height /  2, y: mainSpace.frame.size.width / 2 - 540, width: 30, height: 30))
+var uranus =  UIImageView(frame: CGRect(x: mainSpace.frame.height /  2, y: mainSpace.frame.size.width / 2 - 540, width: 20, height: 20))
 uranus.image = UIImage(named: "uranus.gif")
 uranus.contentMode = UIViewContentMode.scaleAspectFill
 uranus.center.x = sun.center.x
@@ -64,20 +69,23 @@ sun.rotation(view: sun)
 mainSpace.addSubview(mercury)
 mercury.orbit(view: mercury, center: mainSpace, radius: 130.0, time: 1.0)
 mainSpace.addSubview(venus)
-venus.orbit(view: venus, center: mainSpace, radius: 180.0, time: 3.0)
+venus.orbit(view: venus, center: mainSpace, radius: 180.0, time: 2.0)
 mainSpace.addSubview(earth)
-earth.orbit(view: earth, center: mainSpace, radius: 230, time: 5.0)
+earth.orbit(view: earth, center: mainSpace, radius: 230, time: 4.0)
+mainSpace.addSubview(moon)
+moon.orbit(view: moon, center: mainSpace, radius: 230, time: 4.0)
+moon.orbit(view: moon, center: earth, radius: 15, time: 1.0)
 mainSpace.addSubview(mars)
-mars.orbit(view: mars, center: mainSpace, radius: 280, time: 7.0)
+mars.orbit(view: mars, center: mainSpace, radius: 280, time: 6.0)
 mainSpace.addSubview(jupiter)
-jupiter.orbit(view: jupiter, center: mainSpace, radius: 390, time: 9.00)
+jupiter.orbit(view: jupiter, center: mainSpace, radius: 390, time: 8.00)
 mainSpace.addSubview(saturn)
-saturn.orbit(view: saturn, center: mainSpace, radius: 480, time: 11.0)
+saturn.orbit(view: saturn, center: mainSpace, radius: 480, time: 10.0)
 mainSpace.addSubview(uranus)
 uranus.orbit(view: uranus, center: mainSpace, radius: 540, time: 13.0)
 mainSpace.addSubview(neptune)
-neptune.orbit(view: neptune, center: mainSpace, radius: 600, time: 15.0)
+neptune.orbit(view: neptune, center: mainSpace, radius: 600, time: 14.0)
 mainSpace.addSubview(pluto)
-pluto.orbit(view: pluto, center: mainSpace, radius: 650, time: 17.0)
+pluto.orbit(view: pluto, center: mainSpace, radius: 650, time: 20.0)
 
 PlaygroundPage.current.liveView = mainSpace
